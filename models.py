c933856aa808ed4333e73180fdc9683916d3d5ed
Walter Sylvester <walter_sylvester@msn.com>
2019-12-08 10:47:59 -0700

    set up Models

---
+++ models.py
@@ -0,0 +1,57 @@
+import os ##new for heroku
+import datetime
+from peewee import *
+from flask_login import UserMixin
+from playhouse.db_url import connect ##need for heroku
+
+
+if 'ON_HEROKU' in os.environ:
+    DATABASE = connect(os.environ.get('DATABASE_URL')) 
+
+else:
+	DATABASE = SqliteDatabase('issues.sqlite')
+
+
+class User(UserMixin, Model): #User must come before Favorite or you get a "NameError: name 'User' is not defined"
+	screen_name = CharField()
+	email = CharField(unique=True)
+	password_hash = CharField()
+
+	def __str__(self):
+		return '<User: {}, id: {}>'.format(self.email, self.id)
+
+	def __repr__(self):
+		return '<User: {}, id: {}>'.format(self.email, self.id)
+
+	class Meta:
+		db_table = 'users'
+		database = DATABASE
+
+
+class FavoriteTeam(Model):
+	name = CharField()
+	created_at = DateTimeField(default= datetime.date.today())
+	# added created_by to relate a favorite team to the person creating the favorite team
+	created_by = ForeignKeyField(User, backref='favoriteTeams')# Represents One-to-Many
+	user = ForeignKeyField(User)#setting up Many-to-Many relationship
+	team = ForeignKeyField(Team)#setting up Many-to-Many relationship
+
+	class Meta:
+		db_table = 'favoriteTeams'
+		database = DATABASE
+
+
+class Team(Model):
+	name = CharField()
+	
+	class Meta:
+		db_table = 'teams'
+		database = DATABASE
+
+
+
+def initialize():
+	DATABASE.connect()
+	DATABASE.create_tables([Issue, User, Comment], safe=True) 
+	print("TABLES CREATED")
+	DATABASE.close()
